77f1a0a1454aa7d7b1c709bc0e41592442c659a8
Amr Mohamed <eng.amrmohamed95@gmail.com>
2018-11-25 04:02:49 +0200

    new problem

---
+++ 10110.cpp
@@ -0,0 +1,65 @@
+/*
+ *			BATPROBATPROBATPROBATPROBATPROBATPROBATPROBATPROBATPROBATPROBATPR
+ * 			BATPROBATPROBATPROBATPROBATPROBATPROBATPROBATPROBATPROBATPROBATPR
+ * 			BAT.             `PROBATPROBA|\___/|TPROBATPROB'             .ATP
+ * 			BATPR.            `OBATPROBAT|     |PROBATPROBA'           .TPROB
+ * 			BATPRO.            `BATPROBA/       \TPROBATP'            .ROBATP
+ * 			BATPROB.                                                 .ATPROBA
+ * 			BATPROB'                                                 `ATPROBA
+ * 			BATPRO'                                                   `BATPRO
+ * 			BATPROBATPROBATP`---._                     _.---'ROBATPROBATPROBA
+ * 			BATPROBATPROBATPROBATPR--.             .--OBATPROBATPROBATPROBATP
+ * 			BATPROBATPROBATPROBATPROBAT-.       .-PROBATPROBATPROBATPROBATPRO
+ * 			BATPROBATPROBATPROBATPROBATPRO.   .BATPROBATPROBATPROBATPROBATPRO
+ * 			BATPROBATPROBATPROBATPROBATPROB\ /ATPROBATPROBATPROBATPROBATPROBA
+ * 			BATPROBATPROBATPROBATPROBATPROBAVTPROBATPROBATPROBATPROBATPROBATP
+ *
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define SZ(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define ALLR(v) v.rbegin(),v.rend()
+#define FN(s,c) (int)s.find(c)
+#define FOR(i,e) for (int i = 0; i < e; i++)
+#define MFOR(i,s,e) for (int i = s; i <= e; i++)
+#define ROF(i,s) for (int i = s-1; i >= 0; i--)
+#define MROF(i,s,e) for (int i = s; i >= e; i--)
+#define IT(it,v) for(it=v.begin();it!=v.end();it++)
+#define TI(it,v) for(it=v.rbegin();it!=v.rend();it++)
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define FILL(a,v) memset(a,v,sizeof(a))
+
+typedef long long LL;
+typedef vector<int> VI;
+typedef pair<int, int> II;
+/*************************************************/
+
+int main() {
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+//	freopen("out.txt", "w", stdout);
+#endif
+	LL n;
+	while (cin >> n, n) {
+		LL temp = sqrt(n);
+		LL count = temp * 2;
+		if (temp * temp == n) {
+			count--;
+		}
+		if (count % 2 == 0) {
+			cout << "no" << endl;
+		} else {
+			cout << "yes" << endl;
+		}
+	}
+
+	return 0;
+}
+
