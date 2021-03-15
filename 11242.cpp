78570f4ea2bcc538902b635b23d657759be42bd2
Amr Mohamed <eng.amrmohamed95@gmail.com>
2021-03-15 13:14:23 +0200

    new solutions

--- 11242.cpp
+++ 11242.cpp
@@ -19,17 +19,51 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
+bool comp (pair<int, int> &left, pair<int, int> &right) {
+	if (left.first * right.second <= right.first * left.second) {
+		return true;
+	}
+	return false;
+}
 int main() {
 	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 #ifndef ONLINE_JUDGE
 	freopen("in.txt", "r", stdin);
 	freopen("out.txt", "w", stdout);
 #endif
-	unsigned int n;
-	while (cin >> n , n) {
-		unsigned int temp = sqrt(n);
-		cout << (temp * temp == n ? "yes" : "no") << endl;
+	
+	int arr_r[12], arr_f[12];
+	int f;
+	while (cin >> f, f != 0) {
+		int r;
+		cin >> r;
+		for (int i = 0; i < f; i++) {
+			cin >> arr_f[i];
+		}
+		for (int i = 0; i < r; i++) {
+			cin >> arr_r[i];
+		}
+		vector <pair<int, int> > d;
+		for (int i = 0; i < r; i++) {
+			for (int j = 0; j < f; j++) {
+				d.push_back(make_pair(arr_r[i], arr_f[j]));
+			}
+		}
+	
+		sort(d.begin(), d.end(), comp);
+
+		pair<int, int> mx = {d[1].first * d[0].second, d[1].second * d[0].first};
+		for (int i = 2; i < (int)d.size(); i++) {
+			pair<int, int> temp = {d[i].first * d[i-1].second, d[i].second * d[i-1].first};
+			if (comp(mx, temp)) {
+				mx = temp;
+			}
+		}
+		float result = (float)mx.first / mx.second;
+		result = int(result * 100 + 0.5);
+		printf("%.2f\n", result / 100);
+		
 	}
+
 	return 0;
 }
